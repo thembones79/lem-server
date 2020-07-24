aa5eb9cb6df388cb41763ed6f1ea72986efede0a
thembones79 <michal.szumnarski@gmail.com>
2020-07-24 12:56:27 +0200

    changed secret message text

--- router.js
+++ router.js
@@ -7,7 +7,9 @@ const requireSignin = passport.authenticate("local", { session: false });
 
 module.exports = function (app) {
   app.get("/", requireAuth, function (req, res) {
-    res.send({ message: "hi there secret" });
+    res.send({
+      message: "-- this is a secret message stright from the server",
+    });
   });
   app.post("/signin", requireSignin, Auhentication.signin);
   app.post("/signup", Auhentication.signup);
