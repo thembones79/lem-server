9f40bc146c53f4fbca1018947a4951d789de8cc0
thembones79 <michal.szumnarski@gmail.com>
2020-07-24 11:25:03 +0200

    removed config, added env SECRET to authentication.js

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,10 +1,9 @@
 const jwt = require("jwt-simple");
 const User = require("../models/user");
-const config = require("../config");
 
 function tokenForUser(user) {
   const timestamp = new Date().getTime();
-  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+  return jwt.encode({ sub: user.id, iat: timestamp }, process.env.SECRET);
 }
 
 exports.signup = function (req, res, next) {
