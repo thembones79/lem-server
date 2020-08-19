832074d0b5233e007dd523cd30f02a615921d08d
thembones79 <michal.szumnarski@gmail.com>
2020-08-19 23:46:07 +0200

    fix

--- controllers/xlsxSource.js
+++ controllers/xlsxSource.js
@@ -4,7 +4,6 @@ exports.updateMenu = function (req, res, next) {
   const idCode = "menu";
   const menuContent = req.body.menuContent;
   const timeStamp = req.body.timeStamp;
-  console.log({ timeStamp });
 
   XlsxSource.findOne({ idCode: idCode }, function (err, existingMenu) {
     if (err) {
@@ -14,10 +13,10 @@ exports.updateMenu = function (req, res, next) {
     if (!menuContent) {
       return res.status(422).send({ error: "Menu content is missing" });
     }
-    console.log({ existingMenu1: existingMenu });
+
     existingMenu.menuContent = menuContent;
     existingMenu.timeStamp = timeStamp || new Date();
-    console.log({ existingMenu2: existingMenu });
+
     existingMenu.save(function (err) {
       if (err) {
         return next(err);
