9cef3b55740f117fbca7a5723216dd4b07c5286a
thembones79 <michal.szumnarski@gmail.com>
2021-03-16 22:02:30 +0100

    refactored import of keys to require statement to use in conditional logic

--- config/keys.ts
+++ config/keys.ts
@@ -1,10 +1,20 @@
-import { prod } from "./prod";
-import { dev } from "./dev";
-
 export interface IKeys {
   dbConnectionString: string;
   dbAtlas: string;
   secret: string;
 }
 
-export const keys: IKeys = process.env.NODE_ENV === "production" ? prod : dev;
+let keys: IKeys;
+
+const getKeys = (): IKeys => {
+  if (process.env.NODE_ENV === "production") {
+    const prod = require("./prod").prod;
+    return prod;
+  } else {
+    const dev = require("./dev").dev;
+    return dev;
+  }
+};
+
+keys = getKeys();
+export { keys };
