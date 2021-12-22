5d5f58a1e297c30767384249fe86d9ba8fa3ac7e
thembones79 <michal.szumnarski@gmail.com>
2021-12-22 20:44:32 +0100

    adde batchAddAllOrdersStats stub

---
+++ controllers/orderStatistics/batchAddAllOrdersStats.ts
@@ -0,0 +1,60 @@
+import { Request, Response, NextFunction } from "express";
+import { Order } from "../../models/order";
+
+export const batchAddAllOrdersStats = function (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  const orderNumber = req.body.orderNumber;
+  const quantity = req.body.quantity;
+  const partNumber = req.body.partNumber;
+  const qrCode = req.body.qrCode;
+  const tactTime = req.body.tactTime || 36000; // 10 hours
+  const customer = req.body.customer;
+  const orderStatus = "open";
+
+  if (
+    !orderNumber ||
+    !quantity ||
+    !partNumber ||
+    !qrCode ||
+    !tactTime ||
+    !customer
+  ) {
+    res.status(422).send({
+      error: "Not enough values!",
+    });
+    return;
+  }
+  Order.findOne({ orderNumber: orderNumber }, function (err, existingOrder) {
+    if (err) {
+      return next(err);
+    }
+
+    if (existingOrder) {
+      return res.status(422).send({ error: "Order exists" });
+    }
+
+    const order = new Order({
+      orderNumber,
+      quantity,
+      partNumber,
+      qrCode,
+      customer,
+      tactTime,
+      orderStatus,
+      breaks: [],
+      scans: [],
+    });
+
+    order.save(function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.json({
+        order,
+      });
+    });
+  });
+};
